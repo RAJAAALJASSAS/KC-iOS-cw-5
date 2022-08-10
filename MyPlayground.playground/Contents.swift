51db73cda4bf95226060453a72a3226fc47a801b
رجاء الجساس <rajaaljassas2009@gmail.com>
2022-08-10 20:09:56 +0300

    rajaa

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+
+let family = ["Fahad","Maha","Salman","Sara","Dala","Ruba"]
+
+print(family.count)
+
+var evennumbers = [2,4,6,8,10,12]
+evennumbers.remove(at: 1)
+
+evennumbers += [16,18,20,22]
+print(evennumbers.randomElement())
+evennumbers.removeAll()
+var nambers = [1,2,3,4,5]
+for i in nambers
+{
+print(i)
+}
+evennumbers.append(6)
+evennumbers.append(7)
+evennumbers.append(8)
+evennumbers.append(9)
+evennumbers.append(10)
+
+for i in evennumbers {
+    
+    print(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
